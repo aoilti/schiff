6d7c1024988488f58bcbd1d6b71c0c3ace085483
zhangwei <zhangwei2@taou.com>
2021-06-22 17:22:57 +0800

    chore: add wpm

---
+++ .wpmrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  defaultBranch: 'main',
+  allowBranch: ['main', 'v0.*'],
+  bumpFiles: [
+    'package.json',
+    'package-lock.json',
+    {
+      filename: './VERSION.txt',
+      type: 'plain-text',
+    },
+  ],
+  skip: {
+    changelog: true,
+  },
+  commitAll: true,
+  hooks: {
+    prepublish: 'yarn workspaces run build',
+    postpublish: 'lerna publish from-git',
+    postreleaseBranch: 'lerna version {{version}} && git add .',
+  },
+};
